b0516baf91116f9d78666171ae071b9da71c3130
Sanskar Dubey <sanskardubey@playsimple.in>
2025-06-15 13:57:49 +0530

    adding routes file

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -5,4 +5,10 @@ import (
 	"github.com/sanskardubey/Documents/Projects/gocode/src/go-bookstore/pkg/controllers"
 )
 
-var
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.Handlefunc("/book/", controllers.CreateBook).Methods("POST")
+	router.Handlefunc("/book", controllers.GetBook).Methods("GET")
+	router.Handlefunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.Handlefunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.Handlefunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+}
