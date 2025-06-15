1145599236f6e633ac7b99d2690a0b883f6e8d4c
Sanskar Dubey <sanskardubey@playsimple.in>
2025-06-15 14:00:49 +0530

    Fix typo in HandleFunc method calls

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -7,9 +7,9 @@ import (
  
 //RegisterBookStoreRoutes is a function that registers the routes for the bookstore
 var RegisterBookStoreRoutes = func(router *mux.Router) {
-	router.Handlefunc("/book/", controllers.CreateBook).Methods("POST")
-	router.Handlefunc("/book", controllers.GetBook).Methods("GET")
-	router.Handlefunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
-	router.Handlefunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
-	router.Handlefunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
 }
